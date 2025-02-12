18ce38d79d2d1c141562d6ad33a6befa263e7b24
Tvintello <burkow2007@gmail.com>
2025-02-12 14:49:25 +0300

    added requirements

--- cogs/user.py
+++ cogs/user.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
 import discord
-from config import GAME_MODE, EN_RU_DICTIONARY
+from config import GAME_MODE
+import typing
+import wavelink
 
 
 class UserCog(commands.Cog):
@@ -8,14 +10,51 @@ class UserCog(commands.Cog):
         self.bot = bot
 
     @discord.slash_command(description="Turns on game mode to help people communicate with each other")
-    async def turn_on_game_mode(self, ctx: discord.commands.context.ApplicationContext, user: discord.Member):
-        user_name = "".join([EN_RU_DICTIONARY[s] for s in user.name])
+    async def switch_game_mode(self, ctx: discord.commands.context.ApplicationContext, user: discord.Member):
         if user in GAME_MODE:
-            user_name = user_name + "а" if not user_name[-1] in ("а", "е", "и", "о", "у", "ю", "й", "я") else user_name
-            await ctx.respond(f"Игровой режим у {user_name} уже включен")
+
+            await ctx.respond(f"**Отключила** игровой режим для {user.name}")
         else:
             GAME_MODE.append(user)
-            await ctx.respond(f"Включен игровой режим {user_name}")
+            await ctx.respond(f"**Включила** игровой режим для {user.name}")
+
+    @discord.slash_command(description="banger player")
+    async def play_song(self, ctx: discord.commands.context.ApplicationContext, search: str):
+        # First we may define our voice client,
+        # for this, we are going to use typing.cast()
+        # function just for the type checker know that
+        # `ctx.voice_client` is going to be from type
+        # `wavelink.Player`
+        vc = typing.cast(wavelink.Player, ctx.voice_client)
+
+        if not vc:  # We firstly check if there is a voice client
+            try:
+                vc = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+            except AttributeError:
+                return await ctx.respond("Я не могу тебе нашептать на ушко, если ты не находишься в голосовом канале)")
+            except discord.ClientException:
+                await ctx.respond("У меня не получилось подключиться к каналу. Попробуй еще раз вызвать команду!")
+                return
+        print("CONTINUE1")
+
+        # Now we are going to check if the invoker of the command
+        # is in the same voice channel than the voice client, when defined.
+        # If not, we return an error message.
+        if ctx.author.voice.channel.id != vc.channel.id:
+            return await ctx.respond("Ты куда убежал, я вообще-то в другом канале! Как я по твоему "
+                                     "тебе скажу что-то, если ты ушел в другую комнату)")
+        print("CONTINUE2")
+
+        # Now we search for the song. You can optionally
+        # pass the "source" keyword, of type "wavelink.TrackSource"
+        song = await wavelink.Playable.search(search)
+        print("CONTINUE3")
+
+        if not song:  # In case the song is not found
+            return await ctx.respond("Не нашла такой музыки, можно поточнее запрос!")  # we return an error message
+
+        await vc.play(song[0])  # Else, we play it
+        await ctx.respond(f"Играет: `{song[0].title}`")  # and return a success message
 
 
 def setup(bot):
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import dotenv
 import os
 from config import PREFIX, EN_RU_KEYBOARD_DICT, RUS_LETTERS, GAME_MODE
 from scripts.general import GeneralFunctions
+import wavelink
 
 
 dotenv.load_dotenv()
@@ -14,10 +15,36 @@ def run():
     funcs = GeneralFunctions(bot)
     messages = []
 
+    async def connect_nodes():
+        """Connect to our Lavalink nodes."""
+        await bot.wait_until_ready()  # wait until the bot is ready
+
+        nodes = [
+            wavelink.Node(
+                identifier="Node1",  # This identifier must be unique for all the nodes you are going to use
+                uri="http://127.0.0.1:443",
+                # Protocol (http/s) is required, port must be 443 as it is the one lavalink uses
+                password="youshallnotpass"
+            )
+        ]
+
+        await wavelink.Pool.connect(nodes=nodes, client=bot)  # Connect our nodes
+
     async def verify_alien(message: discord.Message) -> bool:
         text = message.content
         return not any([s in text for s in RUS_LETTERS]) and not text.startswith(PREFIX + "en") \
             and message.author in GAME_MODE
+
+    @bot.event
+    async def on_wavelink_node_ready(payload: wavelink.NodeReadyEventPayload):
+        # Everytime a node is successfully connected, we
+        # will print a message letting it know.
+        print(f"Node with ID {payload.session_id} has connected")
+        print(f"Resumed session: {payload.resumed}")
+
+    @bot.event
+    async def on_ready():
+        await connect_nodes()  # connect to the server
 
     @bot.event
     async def on_connect() -> None:
